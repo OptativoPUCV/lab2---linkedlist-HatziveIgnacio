4092d0d986f1aaa56fe6aeaa0811dc9ed82f56d9
Replit user <>
2023-03-23 20:52:02 +0000

    new try

--- list.c
+++ list.c
@@ -71,7 +71,7 @@ void pushFront(List *list, void *data)
      list->head->prev = nuevo; 
   }
   list->head = nuevo;
-  list->tail->next = nuevo;
+  list->tail->next = nuevo->prev;
   nuevo->prev = NULL;
 }
 
