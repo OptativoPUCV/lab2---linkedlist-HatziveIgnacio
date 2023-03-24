e9193098aab8ee031996315080fefa1410559343
Replit user <>
2023-03-24 00:44:35 +0000

    new try

--- list.c
+++ list.c
@@ -83,7 +83,7 @@ void pushBack(List *list, void *data) {
 
 void pushCurrent(List *list, void *data) 
 {
-  if(list->current != NULL);
+  if(list->current != NULL)
     list->current->next->data = data;
 }
 
