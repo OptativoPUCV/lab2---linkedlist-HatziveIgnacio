3a43136267f75a183b56c874c0cfea6b6ab8f292
Replit user <>
2023-03-24 00:25:28 +0000

    new try

--- list.c
+++ list.c
@@ -72,7 +72,7 @@ void pushFront(List *list, void *data)
   }
   list->head = nuevo;
   // No se que tengo que hacer con el tail por el error mencionado en la consola
-  list->tail->next = nuevo;
+  list->tail = nuevo;
   nuevo->prev = NULL;
 }
 
